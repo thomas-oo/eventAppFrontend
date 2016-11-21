015aca12e243548a6bd97e9da228fcc7011d25dd
Oo, Thein <Thein.Oo@KRONOS.com>
2016-11-21 14:44:57 -0500

    Initial Commit

---
+++ eventApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  eventApp
+//
+//  Created by Oo, Thein on 2016-11-21.
+//  Copyright © 2016 Oo, Thein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ eventApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  eventApp
+//
+//  Created by Oo, Thein on 2016-11-21.
+//  Copyright © 2016 Oo, Thein. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
